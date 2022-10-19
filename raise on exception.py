3d0a6de2be3361fed15e9b5d600a81426b9a8a5e
URVIK2004 <95516241+URVIK2004@users.noreply.github.com>
2022-10-19 08:27:03 +0530

    Delete raise on exception.py

--- raise on exception.py
+++
@@ -1,8 +0,0 @@
-try:
-    x=int(input("Enter a Age:-15  "))
-    if x >18:
-        raise ValueError(x)
-except ValueError:
-    print(x,"is grater than 18")
-else:
-    print(x,"is not grater than 18")            
